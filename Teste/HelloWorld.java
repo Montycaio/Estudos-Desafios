362d5a745af8dd78526bd7da03a2c4884f9461c8
Montycaio <caiomonteiro07@icloud.com>
2023-12-14 20:56:31 -0300

    Tipos primitivos

--- Teste/HelloWorld.java
+++ Teste/HelloWorld.java
@@ -1,6 +1,21 @@
 package Teste;
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Java em Teste java");
+        System.out.println("Hello World");
+
+        int valorA = 20;
+
+        int valorB = 20;
+
+        double tipoDouble = 5.5;
+
+        boolean tipoBooleano = true;
+
+        char tipoChar = 'C';
+
+        int total = valorA + valorB;
+
+        System.out.printf("A soma total = %d", total);
+
     }
 }
