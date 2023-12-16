5648c45dbf7e36551e40e68cb009e3fdbaa29426
Montycaio <caiomonteiro07@icloud.com>
2023-12-15 21:38:30 -0300

    Criação do objeto de pagamento

---
+++ Teste/CalcularPagamento.java
@@ -0,0 +1,37 @@
+package Teste;
+
+public class CalcularPagamento {
+  public static void main(String[] args) {
+    try {
+
+      System.out.println("Digite um número");
+      int numeroX = System.in.read();
+      int numeroY = 20;
+
+      alterarsoma(numeroX, numeroY);
+
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.out.println("Erro: " + e.getMessage());
+    }
+
+  }
+
+  // Assinatura
+  private static void alterarsoma(int numeroX, int numeroY) {
+
+    if (numeroX > 0) {
+      numeroX = 100;
+    }
+
+    else {
+      numeroX = 200;
+    }
+
+    int soma = numeroX + numeroY;
+
+    System.out.println(soma);
+
+  }
+
+}
