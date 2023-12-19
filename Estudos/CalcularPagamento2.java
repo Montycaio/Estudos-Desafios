b0b6de48ae4368a97aa129cbe0447850f718fce8
Montycaio <caiomonteiro07@icloud.com>
2023-12-19 18:42:31 -0300

    Estudos

---
+++ Estudos/CalcularPagamento2.java
@@ -0,0 +1,16 @@
+package Estudos;
+
+public class CalcularPagamento2 {
+
+    public static void main(int[], String[] arg){
+
+        System.out.println("Criando textos");
+        
+        int A = 10;
+        int B = 5;
+        
+        soma(A, B);
+
+    }
+    
+}
