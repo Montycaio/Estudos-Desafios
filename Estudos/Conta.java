4cadc9c2982f1a2341e20f3480f45f88be9a5ccf
Montycaio <caiomonteiro07@icloud.com>
2024-01-04 14:44:10 -0300

    2024

---
+++ Estudos/Conta.java
@@ -0,0 +1,16 @@
+package Estudos;
+
+public class Conta {
+    public static void main(String[] args) {
+        double saldo;
+        int agencia;
+        int numero;
+        String titular;
+
+        saldo = 5.50;
+        agencia = 1010;
+        numero = 1407;
+        titular = "roberto";
+    }
+    
+}
