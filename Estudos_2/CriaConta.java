2b619556009b7e5eb02706fb6168b1b1467e35ce
Montycaio <caiomonteiro07@icloud.com>
2024-01-06 13:38:58 -0300

    2024 6 Jan

--- Estudos_2/CriaConta.java
+++ Estudos_2/CriaConta.java
@@ -5,7 +5,7 @@ class CriaConta {
     public static void main(String[] args) {
         
         Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 100;
+        primeiraConta.saldo = 120;
 
         System.out.println("Primeira Conta: " + primeiraConta.saldo);
     }
