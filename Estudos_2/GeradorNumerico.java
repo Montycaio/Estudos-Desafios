82659b0a90a5a17ef6b7da2de8a3634f23febc82
Montycaio <caiomonteiro07@icloud.com>
2024-01-08 16:34:35 -0300

    Gerador Númerico

---
+++ Estudos_2/GeradorNumerico.java
@@ -0,0 +1,15 @@
+package Estudos_2;
+
+import java.util.Random;
+
+public class GeradorNumerico {
+
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+
+        int valor = aleatorio.nextInt(10) +1;
+
+        System.out.println("Número gerado: " + valor);
+    }
+    
+}
