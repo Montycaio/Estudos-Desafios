9c8a35cb1e965400d4a6d107a88ec5091d4d048e
Montycaio <caiomonteiro07@icloud.com>
2024-01-15 18:50:14 -0300

    Estudo de Operadores de Incremento

---
+++ Estudos_3/OperadoresIncrDecr.java
@@ -0,0 +1,26 @@
+package Estudos_3;
+
+public class OperadoresIncrDecr {
+    
+    public static void main(String[] args) {
+        
+        int num = 10;
+        int result = 0;
+
+        System.out.println("Valor original: " + result);
+        result = num++;
+        System.out.println("Valor de num após o incremento: " + num);
+        System.out.println("Valor de result após o incremento: " + result);
+
+        num = 10;
+        result = 0;
+
+        result = ++num;
+        System.out.println("Valor de num após o incremento: " + num);
+        System.out.println("Valor de result após o incremento: " + result);
+
+    }
+
+
+
+}
