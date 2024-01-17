e5057982756ae888024745383d872309c471a289
Montycaio <caiomonteiro07@icloud.com>
2024-01-17 17:09:32 -0300

    Operadores Relacionais

---
+++ Estudos_3/OperadoresRelacionais.java
@@ -0,0 +1,15 @@
+package Estudos_3;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int x = 10;
+        int k =0;
+
+        System.out.printf("x é igual a 10? %b\n", x == 10);
+        System.out.printf("x é diferente de 10? %b\n", x != 10 );
+        System.out.printf("x é maior que k? %b\n", x > k);
+        System.out.printf("x é maior ou igual a 10? %b\n", x >= 10);
+        System.out.printf("x é menor que 10? %b\n", x < 10);
+        System.out.printf("x é menor ou igual a k? %b\n", x <= k);
+    }
+}
