a3c23c276b8bab3dec447f9a4d8c92b0e7411c4f
Montycaio <caiomonteiro07@icloud.com>
2024-01-24 20:17:19 -0300

    Criando um login simples

---
+++ Estudos_3/Login.java
@@ -0,0 +1,30 @@
+package Estudos_3;
+
+import java.util.Scanner;
+
+public class Login {
+    public static final String Caio = null;
+
+    public static void main(String[] args) {
+        String log;
+        int password;
+        Scanner texto = new Scanner(System.in);
+        
+
+        System.out.println("Login:");
+        log = texto.nextLine();
+
+        System.out.println("Seja bem-vindo " + log + "\ninforme sua senha, porfavor.");
+
+        password = texto.nextInt();
+
+        if (password == 2405){
+        System.out.println("Acesso Autorizado!");
+        }
+        else {
+        System.out.println("Acesso Negado!");
+        }
+
+        texto.close();
+    }
+}
