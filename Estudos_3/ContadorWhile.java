f1478d714e6c03bc8bd9315c581d84163cf680fd
Montycaio <caiomonteiro07@icloud.com>
2024-01-26 23:20:07 -0300

    Estudando condicional While

---
+++ Estudos_3/ContadorWhile.java
@@ -0,0 +1,18 @@
+package Estudos_3;
+
+public class ContadorWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        System.out.println("Contando até 50: \n");
+
+        while (contador <= 50) {
+            System.out.println(contador);
+            contador++;
+        }
+
+        System.out.println("\nConcluido!");
+    }
+
+    
+}
