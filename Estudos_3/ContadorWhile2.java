124af141f37f4a24268a8e756bde82edb4fd5039
Montycaio <caiomonteiro07@icloud.com>
2024-01-27 16:15:18 -0300

    Utilizando equals para comparar String

---
+++ Estudos_3/ContadorWhile2.java
@@ -0,0 +1,23 @@
+package Estudos_3;
+
+import java.util.Scanner;
+
+public class ContadorWhile2 {
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Digite seu nome: ");
+        nome = Entrada.nextLine();
+
+
+        while (!nome.equals("s")) {
+            System.out.println("Seja-bem vindo " + nome);
+            System.out.println("Digite seu nome ou s para sair");
+            nome = Entrada.nextLine();
+        }
+
+        Entrada.close();
+        
+    }
+}
