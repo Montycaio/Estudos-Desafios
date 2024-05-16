fbc62c19d733bb2e91c540c5f24c6bb2e8cd8ac9
Montycaio <caiomonteiro07@icloud.com>
2024-05-15 23:05:28 -0300

    Estudo em C

--- Estudos_4/Atividades/output/aula7.c
+++ Estudos_4/Atividades/output/aula7.c
@@ -3,7 +3,32 @@
 
 int main()
 {
+    int a;
+    int b;
 
+    printf("Digite o valor de \"A\":");
+    scanf("%d", &a);
+    getchar();
+
+    printf("Digite o valor de \"B\":");
+    scanf("%d", &b);
+    getchar();
+
+    if (a < b)
+    {
+        printf("\nResp: A menor que B\n\n");
+    }
+
+    else if (a > b)
+    {
+        printf("\nResp: A maior que B\n\n");
+    }
+
+    else
+    {
+        printf("\nResp: A igual B\n\n");
+    }
+    
     
 
     system("PAUSE");
