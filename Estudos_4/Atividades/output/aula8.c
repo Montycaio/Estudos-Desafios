be474c2e2ff55c529d3c805102bfc1eb48448c23
Montycaio <caiomonteiro07@icloud.com>
2024-05-16 23:12:11 -0300

    Conversor de bases numéricas em C

--- Estudos_4/Atividades/output/aula8.c
+++ Estudos_4/Atividades/output/aula8.c
@@ -3,5 +3,38 @@
 
 int main()
 {
+    int opcao, valor;
+
+    printf("\nConversor Bases Numericas\n\n");
+    printf("1: Decimal para Hexadecimal\n");
+    printf("2: Hexadecimal para Decimal\n");
+    printf("\n\nInforme a opcao:#");
+    scanf("%d", &opcao);
+    getchar();
+
+    if (opcao == 1)
+    {
+        printf("Informe um numero:");
+        scanf("%d", &valor);
+        getchar();
+        printf("\n\"%d\" em Hexadecimal sera: \"%x\"\n\n.",valor,valor);
+    }
+
+   else if (opcao ==2)
+    {
+        printf("Informe um numero:");
+        scanf("%x", &valor);
+        getchar();
+        printf("\n\"%x\" em Decimal sera: \"%d\"\n\n.",valor,valor);
+
+    }
+
+    else
+    {
+        printf("\nOpcao Invalida.\n\n");
+    }
     
+
+    system("PAUSE");
+    return 0;
 }
