c97b1beb2554af9fb3979c792f70fe6e6d70c387
Montycaio <caiomonteiro07@icloud.com>
2024-08-15 21:29:56 -0300

    Teste Python

---
+++ Python/Estudos/1.py
@@ -0,0 +1,33 @@
+
+
+# variaveis e tipos de dados
+idade = 25
+nome = "Alberto"
+altura = 1, 68
+
+# Operadores
+
+print(10+5)
+
+print(10-5)
+
+print(10*5)
+
+print(10/5)
+
+print(10 > 5)
+
+print(10 < 5)
+
+# Estrutura
+
+# Pergunta o nome da pessoa
+nome = input("Qual é o seu nome?:")
+
+# Exibe uma mensagem de boas-vindas
+print(f"Olá, {nome}! Prazer em conhecê-lo.")
+
+
+
+
+
